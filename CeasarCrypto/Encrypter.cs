3896ba947f81dfa04e928d0b053f1120c2e77d33
Jesper Klejs Deleurand (JESP446C) <jesperd55@gmail.com>
2021-01-04 10:21:36 +0100

    Initial commit

---
+++ CeasarCrypto/Encrypter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CeasarCrypto
+{
+    class Encrypter
+    {
+
+        public static string Encrypt(string msg)
+        {
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            msg = msg.ToUpper();
+            string alphabet2 = "DEFGHIJKLMNOPQRSTUVWXYZABC";
+
+            string newString = "";
+
+            foreach (Char item in msg)
+            { 
+                foreach(char letter in alphabet)
+                {
+                    if (item == letter)
+                    {
+
+                        int alphIndex = alphabet.IndexOf(letter);
+
+                        newString = newString + (alphabet2.ElementAt(alphIndex));
+                    }
+                }
+            }
+
+            return newString;
+        }
+
+        public static string Decrypt(string msg)
+        {
+
+            string alphabet = "DEFGHIJKLMNOPQRSTUVWXYZABC";
+            string alphabet2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            msg = msg.ToUpper();
+
+            string newString = "";
+
+            foreach (Char item in msg)
+            {
+                foreach (char letter in alphabet)
+                {
+                    if (item == letter)
+                    {
+
+                        int alphIndex = alphabet.IndexOf(letter);
+
+                        newString = newString + (alphabet2.ElementAt(alphIndex));
+                    }
+                }
+            }
+
+            return newString;
+        }
+    }
+}
---
+++ CeasarCrypto/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CeasarCrypto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cæsar Kryptering:");
+            Console.WriteLine();
+            Console.WriteLine("Krpteret: " + Encrypter.Encrypt("Abbekat"));
+            Console.WriteLine("Dekrypteret: " + Encrypter.Decrypt(Encrypter.Encrypt("Abbekat")));
+
+            Console.ReadKey();
+        }
+    }
+}
